b35244ad362d73290256f2bfb9670c1e171cc15d
Flamen_Dialis <menendezrfelixj@gmail.com>
2020-06-30 11:48:58 -0300

    Updates

---
+++ precache-manifest.7f60ce2bbfbc1473e3c11852c6a1a611.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625f74f58bc125ef2bd4c683539e3c83",
+    "url": "/liteflix/index.html"
+  },
+  {
+    "revision": "4b113576e42aa30db98c",
+    "url": "/liteflix/static/css/main.72d698c2.chunk.css"
+  },
+  {
+    "revision": "9d451da2ba9e8177a1de",
+    "url": "/liteflix/static/js/2.73857bf9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/liteflix/static/js/2.73857bf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b113576e42aa30db98c",
+    "url": "/liteflix/static/js/main.6ebdde2f.chunk.js"
+  },
+  {
+    "revision": "0c860857470774dd22d8",
+    "url": "/liteflix/static/js/runtime-main.3eb919ba.js"
+  },
+  {
+    "revision": "e5cebfd3bead86a37411b7414bc5e640",
+    "url": "/liteflix/static/media/add-list.e5cebfd3.svg"
+  },
+  {
+    "revision": "b4d4884df02fbe8e4326603f1e4c9239",
+    "url": "/liteflix/static/media/arrow.b4d4884d.svg"
+  },
+  {
+    "revision": "42d9b0eb98182d389d3d0195f9607f59",
+    "url": "/liteflix/static/media/bell.42d9b0eb.svg"
+  },
+  {
+    "revision": "14b9bd2c26b12232912d569fbcd7f6c6",
+    "url": "/liteflix/static/media/clip.14b9bd2c.svg"
+  },
+  {
+    "revision": "f5be77f218c182797090d6a4a3589867",
+    "url": "/liteflix/static/media/fill-1.f5be77f2.svg"
+  },
+  {
+    "revision": "6b16afd6b4ea6080431b0fcf66c0db83",
+    "url": "/liteflix/static/media/fill-2.6b16afd6.svg"
+  },
+  {
+    "revision": "94d1bb474ee8a41607350f516b64b8ac",
+    "url": "/liteflix/static/media/like.94d1bb47.svg"
+  },
+  {
+    "revision": "60daaf3f4c7214e568ed847352b800e4",
+    "url": "/liteflix/static/media/liteflix.60daaf3f.svg"
+  },
+  {
+    "revision": "393d4c6103b1a0ae7186dd1d68f20104",
+    "url": "/liteflix/static/media/play.393d4c61.svg"
+  },
+  {
+    "revision": "be78363e848bc9c362fe63c8c24dd2bf",
+    "url": "/liteflix/static/media/plus.be78363e.svg"
+  },
+  {
+    "revision": "2084d69fe90f1065a069ab573f157120",
+    "url": "/liteflix/static/media/thin-arrow.2084d69f.svg"
+  },
+  {
+    "revision": "7d31052a78b4a494ea8bccd200c040d1",
+    "url": "/liteflix/static/media/user-01.7d31052a.svg"
+  }
+]);
